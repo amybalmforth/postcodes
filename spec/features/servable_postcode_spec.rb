bc76cabbdb79699965977b501e22a6e7e57b41f7
Amy Balmforth <amy.balmforth@gmail.com>
2020-02-25 18:04:30 +0000

    capybara test passed for servable postcode

--- spec/features/servable_postcode_spec.rb
+++ spec/features/servable_postcode_spec.rb
@@ -3,4 +3,11 @@ feature 'servable postcode' do
     visit('/')
     expect(page).to have_content('postcodes.')
   end
+
+  scenario 'entering a servable postcode' do
+    visit('/')
+    fill_in 'my_postcode', with: 'SE17QD'
+    click_button('go')
+    expect(page).to have_content('This postcode is servable')
+  end
 end
