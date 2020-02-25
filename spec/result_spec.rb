b59352bfb8872a444e03b4a254d94951a7c34ec4
Amy Balmforth <amy.balmforth@gmail.com>
2020-02-25 08:27:52 +0000

    show_data method also shows hash

--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -4,7 +4,7 @@ require './models/postcode'
 describe Result do
   let(:postcode) { Postcode.new('SG47BH') }
   let(:result) { described_class.new(postcode) }
-  my_hash = {"status"=>200, "result"=>{"postcode"=>"SG4 7BH", "quality"=>1, "eastings"=>525799, "northings"=>229833, "country"=>"England", "nhs_ha"=>"East of England", "longitude"=>-0.17086, "latitude"=>51.952768, "european_electoral_region"=>"Eastern", "primary_care_trust"=>"Hertfordshire", "region"=>"East of England", "lsoa"=>"North Hertfordshire 005G", "msoa"=>"North Hertfordshire 005", "incode"=>"7BH", "outcode"=>"SG4", "parliamentary_constituency"=>"North East Hertfordshire", "admin_district"=>"North Hertfordshire", "parish"=>"Weston", "admin_county"=>"Hertfordshire", "admin_ward"=>"Weston and Sandon", "ced"=>"Royston West & Rural", "ccg"=>"NHS East and North Hertfordshire", "nuts"=>"Hertfordshire", "codes"=>{"admin_district"=>"E07000099", "admin_county"=>"E10000015", "admin_ward"=>"E05004784", "parish"=>"E04012229", "parliamentary_constituency"=>"E14000845", "ccg"=>"E38000049", "ccg_id"=>"06K", "ced"=>"E58000659", "nuts"=>"UKH23"}}}
+  my_hash = { 'status' => 200, 'result' => { 'postcode' => 'SG4 7BH', 'quality' => 1, 'eastings' => 525_799, 'northings' => 229_833, 'country' => 'England', 'nhs_ha' => 'East of England', 'longitude' => -0.17086, 'latitude' => 51.952768, 'european_electoral_region' => 'Eastern', 'primary_care_trust' => 'Hertfordshire', 'region' => 'East of England', 'lsoa' => 'North Hertfordshire 005G', 'msoa' => 'North Hertfordshire 005', 'incode' => '7BH', 'outcode' => 'SG4', 'parliamentary_constituency' => 'North East Hertfordshire', 'admin_district' => 'North Hertfordshire', 'parish' => 'Weston', 'admin_county' => 'Hertfordshire', 'admin_ward' => 'Weston and Sandon', 'ced' => 'Royston West & Rural', 'ccg' => 'NHS East and North Hertfordshire', 'nuts' => 'Hertfordshire', 'codes' => { 'admin_district' => 'E07000099', 'admin_county' => 'E10000015', 'admin_ward' => 'E05004784', 'parish' => 'E04012229', 'parliamentary_constituency' => 'E14000845', 'ccg' => 'E38000049', 'ccg_id' => '06K', 'ced' => 'E58000659', 'nuts' => 'UKH23' } } }
   before do
     allow(postcode).to receive(:data).and_return(my_hash)
   end
@@ -17,6 +17,9 @@ describe Result do
   describe 'show_data method' do
     it 'shows ruby hash as data' do
       expect(postcode.data).to eq my_hash
+    end
+    it 'shows ruby hash with show_data method' do
+      expect(result.show_data).to eq my_hash
     end
   end
 end
